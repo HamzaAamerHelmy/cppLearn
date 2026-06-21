66c8c38b0b67173fa35a0252bd0a5b483399d0a1
Hamza Aamer <Hamza.Aamer@outlook.com>
2026-06-21 21:15:47 +0300

    refactor functions to improve parameter handling and streamline array operations

--- main.cpp
+++ main.cpp
@@ -3,87 +3,54 @@
 #include <cstdlib>
 using namespace std;
 
-int readNumberOfElements()
+void readNumberOfElements(int &arrlength)
 {
-    int numberOfElements;
-    cout << "Enter Number Of Elements Do you want from 1 to 100: ";
-    cin >> numberOfElements;
-    return numberOfElements;
+    cout << "Enter Number Of Elements: ";
+    cin >> arrlength;
 }
-void fillArrayWithRandomNumbers(int numberOfElements, int arr[])
+void fillArrayWithRandomNumbers(int arr[100], int arrlength)
 {
-    for (int i = 0; i < numberOfElements; i++)
+    for (int i = 0; i < arrlength; i++)
     {
         arr[i] = rand() % (100 - 1) + 1;
     }
 }
-void printArray(int numberOfElements, int arr[])
+void addArrayElement(int arr[], int number, int &arrlength)
 {
-    for (int i = 0; i < numberOfElements; i++)
+    arrlength++;
+    arr[arrlength - 1] = number;
+}
+void copyArray(int arr2[], int arr[], int &arr2length, int arrlength)
+{
+    for (int i = 0; i < arrlength; i++)
+    {
+        addArrayElement(arr2, arr[i], arr2length);
+    }
+}
+void printArray(int arr[], int arrlength)
+{
+    for (int i = 0; i < arrlength; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl
          << endl;
 }
-int readNumberToSearch()
-{
-    int numberToSearch;
-    cout << "Enter Number To Search On It: ";
-    cin >> numberToSearch;
-    return numberToSearch;
-}
-int getIndexOfNumber(int numberToSearch, int arr[], int numberOfElements)
-{
-    for (int i = 0; i < numberOfElements; i++)
-    {
-        if (arr[i] == numberToSearch)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-bool isExist(int numberToSearch, int position, int arr[])
-{
-    if (position <= 0)
-    {
-        return false;
-    }
-    return true;
-}
-void printExist(int position, int numberOfElements, int arr[], int numberToSearch)
-{
-    int orderOfNumber = position + 1;
-    cout << "\nNumber you are Looking For is: " << numberToSearch << endl
-         << endl;
-    if (isExist(numberToSearch, position, arr))
-    {
-        cout << "The Number Is Found :)" << endl;
-    }
-    else
-    {
-        cout << "The Number Is Not Found :(" << endl;
-    }
-}
 
 void RunApp()
 {
     srand((unsigned)time(NULL));
+    int arr[100], arrlength;
+    int arr2[100], arr2length = 0;
+    readNumberOfElements(arrlength);
 
-    int numberOfElements = readNumberOfElements();
+    fillArrayWithRandomNumbers(arr, arrlength);
+    cout << "Array 1 Elements: " << endl;
+    printArray(arr, arrlength);
 
-    int arr[numberOfElements];
-
-    fillArrayWithRandomNumbers(numberOfElements, arr);
-
-    printArray(numberOfElements, arr);
-
-    int numberToSearch = readNumberToSearch();
-
-    int position = getIndexOfNumber(numberToSearch, arr, numberOfElements);
-
-    printExist(position, numberOfElements, arr, numberToSearch);
+    copyArray(arr2, arr, arr2length, arrlength);
+    cout << "Array 2 Elements: " << endl;
+    printArray(arr2, arrlength);
 }
 
 int main()
