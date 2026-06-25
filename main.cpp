73fc5367264be119e0b1a23a6cf7066776f80168
Hamza Aamer <Hamza.Aamer@outlook.com>
2026-06-25 16:31:06 +0300

    remove unused functions and streamline RunApp for improved clarity and functionality

--- main.cpp
+++ main.cpp
@@ -5,101 +5,16 @@
 #include <chrono>
 using namespace std;
 
-void readNumberOfElements(int &arrlength)
-{
-    cout << "How many random numbers do you want to generate: ";
-    cin >> arrlength;
-}
-
-void readNumbersFromAndTo(int &from, int &to)
-{
-    cout << "Enter The Number Will Start From it: ";
-    cin >> from;
-    cout << "Enter the Number Will End From it: ";
-    cin >> to;
-}
-
-void fillArrayWithRandomNumbers(int arr[], int arrlength, int from, int to)
-{
-    for (int i = 0; i < arrlength; i++)
-    {
-        arr[i] = rand() % (to - from) + from;
-    }
-}
-
 void addArrayElement(int arr[], int number, int &arrlength)
 {
     arrlength++;
     arr[arrlength - 1] = number;
 }
 
-bool isPrime(int number)
-{
-    for (int i = 2; i <= (number / 2); i++)
-    {
-        if (number % i == 0)
-        {
-            return false;
-        }
-    }
-    if (number <= 1)
-    {
-        return false;
-    }
-    return true;
-}
-
-void copyPrimeNumbersFromArray(int arr2[], int arr[], int &arr2length, int arrlength)
-{
-    int counter = 0;
-    for (int i = 0; i < arrlength; i++)
-    {
-        if (isPrime(arr[i]))
-        {
-            addArrayElement(arr2, arr[i], counter);
-        }
-    }
-    arr2length = counter;
-}
-
-void printArray(int arr[], int arrlength)
-{
-    for (int i = 0; i < arrlength; i++)
-        cout << arr[i] << " ";
-
-    cout << endl
-         << endl;
-}
 
 void RunApp()
 {
-    // Seed Random Number Generator
-    srand((unsigned)time(NULL));
-
-    // Assign Original Array Length
-    int arrlength = 0;
-    readNumberOfElements(arrlength);
-
-    int from, to;
-    readNumbersFromAndTo(from, to);
-
-    // Fill Original Array
-    int arr[arrlength];
-    fillArrayWithRandomNumbers(arr, arrlength, from, to);
-
-    // Print Origin Array Elemnts
-    cout << "\nGenerating " << arrlength << " Random Numbers . . . " << endl;
-    this_thread::sleep_for(chrono::seconds(3));
-    printArray(arr, arrlength);
     
-    // Extract Prime Numbers From origin Array to new Array
-    int arr2[100], arr2length = 0;
-    copyPrimeNumbersFromArray(arr2, arr, arr2length, arrlength);
-    
-    // Print Prime's Array
-    cout << "\nExtracting Prime Numbers . . . " << endl;
-    this_thread::sleep_for(chrono::seconds(3));
-    printArray(arr2, arr2length);
 }
 
 int main()
