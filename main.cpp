878be5b8fd3899abd130ecd06b0d9102dab2d3f8
Hamza Aamer <Hamza.Aamer@outlook.com>
2026-06-25 13:10:15 +0300

    simplify condition in copyPrimeNumbersFromArray function

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ void copyPrimeNumbersFromArray(int arr2[], int arr[], int &arr2length, int arrle
 {
     for (int i = 0; i < arrlength; i++)
     {
-        if (isPrime(arr[i]) == true)
+        if (isPrime(arr[i]))
         {
             addArrayElement(arr2, arr[i], arr2length);
         }
